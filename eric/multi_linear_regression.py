9205c808213878309712b4ad56147909f26a167f
eclam <lameric1994@gmail.com>
2018-06-06 14:54:09 -0700

    multi-linear regression

--- eric/multi_linear_regression.py
+++ eric/multi_linear_regression.py
@@ -1,12 +1,16 @@
 import numpy as np
+from scipy import stats
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
 # import geoip2.database
+#
+plt.clf()
 
 DATA_ROOT = '../data'
+CSV_FILE_NAME = 'locn-filtered' + '.csv'
 CSV_FILE_PATH = os.path.join(DATA_ROOT,'/home/eric/Documents/ubc_workshop/pims-bcdata18-cloudpbx/data/' +
-		'127.0.0.1-2018-05-2018-3-58 PM- voipmonitor-cdr.csv')
+		CSV_FILE_NAME)
 
 DESCRIBED_COLUMNS = ["calldate", "callend", "duration", "connect_duration", "progress_time", 
                      "first_rtp_time", "caller", "caller_domain", "caller_reverse", "callername", 
@@ -26,42 +30,86 @@ DESCRIBED_COLUMNS = ["calldate", "callend", "duration", "connect_duration", "pro
                      "b_packet_loss_perc_mult1000", "delay_sum", "a_delay_sum", "b_delay_sum", 
                      "a_rtp_ptime", "b_rtp_ptime"]
 
-# read dataframe. calldate and callend are the only date columns
 df = pd.read_csv(CSV_FILE_PATH, parse_dates=['calldate', 'callend'])
 df = df[DESCRIBED_COLUMNS]
-# remo`ve any columns that are entirely NA.
 df.dropna(axis=1, how='all', inplace=True)
-# some columns have "sanitized" written the whole way down:
-#    remove these.
 df = df.loc[:, ~(df == 'sanitized').all(axis=0)]
-# ## sighup was removed by the DESCRIBED_COLUMNS subsetting
-# XX sighup column is entirely zeros. remove it. 
-# XX df.drop(columns=['sighup'], inplace=True) 
-# create a version of duration column of type timedelta
 df.drop(columns=[col for col in df.columns if 'reverse' in col], inplace=True)
 df['duration_td'] = df.callend - df.calldate
-
-# a dataframe where the rows have been removed if 
-# there are more than 60% NA values in those rows.
 df_dens = df.dropna(axis=0, thresh = 79)
-
-bins = [-1, 1, 30, 35, 40, 45]
-# df.dropna(subset=['a_saddr', 'b_saddr'])
 df = df[ df.connect_duration > 0 ]
 df = df[ df.a_saddr > 0 ] 
 df = df[ df.b_saddr > 0 ]
+# df['a_mos_adapt_mult10'].fillna(0)
+# df['b_mos_adapt_mult10'].fillna(0)
+# df['a_mos_f1_mult10'].fillna(0)
+# df['a_mos_f2_mult10'].fillna(0)
+# df['b_mos_f1_mult10'].fillna(0)
+# df['b_mos_f2_mult10'].fillna(0)
 '''
        We do > 0 because of the possibility of hackers just hanging up at 0:00:00 
        time of the call. Having != 0 just dirties out data because it adds
        the hacker data
 '''
+'''
+       # multi-linear regression
+       # 
+       # flatten packetloss into 1 number (Predictan)
+       # decide on predictors ( delay_sum, jitter, mos)
+       # mos -> could get a better predictor later from better results
 
-f1 =df
-f1['a_mos_adapt_mult10'].fillna(0)
-f1['b_mos_adapt_mult10'].fillna(0)
-f1['a_mos_f1_mult10'].fillna(0)
-f1['a_mos_f2_mult10'].fillna(0)
-f1['b_mos_f1_mult10'].fillna(0)
-f1['b_mos_f2_mult10'].fillna(0)
+'''
 
-print(f1)
+#### procure data -> Delay_Sum, Jitter, MOS
+sum_pkt_loss_a =  df['a_sl1'] + df['a_sl2'] + df['a_sl3'] +\
+      df['a_sl4'] + df['a_sl5'] + df['a_sl6'] + df['a_sl7'] + \
+      df['a_sl8'] + df['a_sl9'] + df['a_sl10'] 
+
+sum_pkt_loss_b =  df['b_sl1'] + df['b_sl2'] + df['b_sl3'] +\
+      df['b_sl4'] + df['b_sl5'] + df['b_sl6'] + df['b_sl7'] + \
+      df['b_sl8'] + df['b_sl9'] + df['b_sl10'] 
+
+avg_pkt_loss= (sum_pkt_loss_a + sum_pkt_loss_b)/2
+# avg_pkt_loss = optional b/c 0's can be in either row for the same thing
+
+
+xi = df[['delay_sum','a_maxjitter','b_maxjitter','a_mos_adapt_mult10',\
+       'b_mos_adapt_mult10', 'a_mos_f1_mult10', \
+       'a_mos_f2_mult10', 'b_mos_f1_mult10', 'b_mos_f2_mult10']].values
+x = np.zeros((xi.shape[0], xi.shape[1]+1))
+x[:,:-1] = xi
+x[:,-1] = np.ones(xi.shape[0])
+
+# row = df.iloc[0]
+# print (row)
+# print(sum_pkt_loss_a)
+# print(sum_pkt_loss_b)
+# print(avg_pkt_loss)
+y = sum_pkt_loss_a.values
+#print(x.shape)
+#print(y.shape)
+m= np.linalg.lstsq(x,y)
+# print(m[2])
+# print(m[0])
+#yfit = np.zeros_like(y)
+#yfit = m[0] * np.transpose(x) #+ m[1]
+yfit = x.dot(m[0])
+#for i, slope in enumerate(m[0]):
+#       yfit = yfit +(slope * x[:,i] + m[2])
+       #print('M:{} b:{}'.format(tup[0], tup[1]))
+
+#yfit = yfit + m[1]
+
+print (yfit.shape, y.shape, m[0].shape, x.shape)
+i = range(len(y))
+print(m[0])
+plt.plot(i,y,'ro')
+plt.plot(i,yfit,':')
+plt.show()
+
+# slope, intercept, r_value, p_value, std_err = stats.linregress(x,sum_pkt_loss_a)
+# print(slope)
+# print(intercept)
+# print(r_value)
+# print(p_value)
+# print(std_err)
